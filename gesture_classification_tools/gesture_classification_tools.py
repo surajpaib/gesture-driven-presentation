f01a4bd3a00a3106d1339e2a18770dbb0a5a4ed2
ob <onurbil@hotmail.com>
2020-04-01 19:56:42 +0200

    print train test accuracy

--- gesture_classification_tools/gesture_classification_tools.py
+++ gesture_classification_tools/gesture_classification_tools.py
@@ -1,14 +1,13 @@
 # import xmltodict
 import numpy as np
 # import os
-
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from xml_processing_tools import *
-
+from debugging_tools import *
 
 def evaluate_model(trainX, trainy, testX, testy):
     verbose, epochs, batch_size = 0, 15, 64
@@ -22,21 +21,29 @@ def evaluate_model(trainX, trainy, testX, testy):
     # fit network
     model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
     # evaluate model
-    _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
-    return accuracy
+    _, train_accuracy = model.evaluate(trainX, trainy, batch_size=batch_size, verbose=0)
+    _, test_accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+    return train_accuracy, test_accuracy
 
 
 
 X,Y = xmlToNumpy()
 
 repeats = 2
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
 scores = list()
 for r in range(repeats):
-    score = evaluate_model(X_train, y_train, X_test, y_test)
-    score = score * 100.0
-    print('>#%d: %.3f' % (r + 1, score))
-    scores.append(score)
+    train_test_scores = evaluate_model(X_train, y_train, X_test, y_test)
+    train_score = train_test_scores[0]
+    test_score = train_test_scores[1]
+    train_score = train_score * 100.0
+    test_score = test_score * 100.0
+
+    print('train score: >#%d: %.3f' % (r + 1, train_score))
+    print('test score: >#%d: %.3f' % (r + 1, test_score))
+    scores.append(train_score)
+    scores.append(test_score)
+
 
 debug(scores)
 # summarize_results(scores)
