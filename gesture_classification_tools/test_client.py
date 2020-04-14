07bec5f1baf5c69829209db9a6992f76e191508c
ob <onurbil@hotmail.com>
2020-04-14 19:33:15 +0200

    Comments for test_client added

--- gesture_classification_tools/test_client.py
+++ gesture_classification_tools/test_client.py
@@ -3,24 +3,39 @@ import requests
 import json
 import numpy as np
 
+
 """
 Testing of keras serving model:
 """
 
-def testModelServer():
+def testModelServer(array):
     """
     Function to test the tensorflow serving model.
+    array: Numpy array of shape (1,120,12)
+    120: frame test_size If frame is size smaller zeros can be added to the
+    end. 20 frames should be ok.
+    12: x an y points of body pose. The order must be consistent:
+    
+    [left shoulder x, left shoulder y, left elbow x, left elbow y,
+     left wrist x, left wrist y, right shoulder x, right shoulder y,
+     right elbow x, right elbow y, right wrist x, right wrist y]
+
+    predictions: list in list output. The order is:
+    [[lprev, reset, rnext, startstop]]
     """
-    # An array to test the serving model:
-    test_array = np.arange(1440).reshape([1,120,12])
-
-    data = json.dumps({"signature_name": "serving_default", "instances": test_array.tolist()})
+    data = json.dumps({"signature_name": "serving_default",
+                       "instances": array.tolist()})
     headers = {"content-type": "application/json"}
-    json_response = requests.post('http://localhost:9000/v1/models/saved_model/versions/1:predict', data=data, headers=headers)
+    json_response = requests.post(
+    'http://localhost:9000/v1/models/saved_model/versions/1:predict',
+    data=data, headers=headers)
     predictions = json.loads(json_response.text)['predictions']
-    print(predictions)
 
-    return None
+    return predictions
 
 
-testModelServer()
+# An array to test the serving model:
+test_array = np.arange(1440).reshape([1,120,12])
+
+model_prediction = testModelServer(test_array)
+print(model_prediction)
